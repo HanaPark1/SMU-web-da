8a224b126dcc95523d3e7514e5f03416261a302c
HanaPark1 <ydjxlst@gmail.com>
2025-05-19 18:36:24 +0900

    study: pandas, DF

---
+++ Day5/0519_02.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#1차원 데이터 | 1개 컬럼, 여러개 index
+# temp = pd.Series([-20,-10,0,10,20],index = ['1월','2월','3월','4월','5월'])
+# print(temp)
+
+# print(temp['1월'])
+
+# print(temp.sum())
+
+# 2차원 데이터 : DataFrame(DF) | 여러 개 컬럼, 여러개 index 구성
+# 데이터 구성 : 딕셔너리에 리스트 타입으로 구성됨
+
